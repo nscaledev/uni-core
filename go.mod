unikorn-cloud/core

Service Messaging
Services may need to provision infrastructure in their own namespace on
behalf of a user.  This is primarily a security boundary so a user does
not have access to the underlying infrastructure (e.g. via a kubeconfig
for a Kubernetes based service), due to elevated privileges for example.

However we want the user (principal) to be available in the Kubernetes
service so that it correctly attributes the resource allocations to the
right organization and project.  For that we need a secure mechanism to
relay principal identity information to other services.  Luckily, as all
services talk to one another with mTLS, we already have a secure key
distribution mechanism that end users cannot use (they don't have access
to the CA's private key, so cannot invent identities that can be
trusted).  Thus we provide a couple interfaces that allow arbitrary data
to be encoded and signed into a suitable form for injection as a HTTP
header, and allow a HTTP header to be verified and decoded again on the
other end.

go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Masterminds/semver/v3 v3.3.1
 	github.com/brunoga/deep v1.2.4
 	github.com/getkin/kin-openapi v0.132.0
+	github.com/go-jose/go-jose/v3 v3.0.4
 	github.com/go-logr/logr v1.4.2
 	github.com/go-openapi/jsonpointer v0.21.1
 	github.com/spf13/pflag v1.0.6
@@ -71,6 +72,7 @@ require (
 	go.opentelemetry.io/proto/otlp v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect

pkg/client/http.go
@@ -18,10 +18,13 @@ package client
 
 import (
 	"context"
+	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
 
+	jose "github.com/go-jose/go-jose/v3"
 	"github.com/spf13/pflag"
 
 	"github.com/unikorn-cloud/core/pkg/errors"
@@ -106,6 +109,35 @@ func (o *HTTPClientOptions) AddFlags(f *pflag.FlagSet) {
 	f.StringVar(&o.secretName, "client-certificate-name", o.secretName, "Client certificate secret name.")
 }
 
+func (o *HTTPClientOptions) loadTLSCertificate(ctx context.Context, cli client.Client) (*tls.Certificate, error) {
+	secret := &corev1.Secret{}
+
+	if err := cli.Get(ctx, client.ObjectKey{Namespace: o.secretNamespace, Name: o.secretName}, secret); err != nil {
+		return nil, err
+	}
+
+	if secret.Type != corev1.SecretTypeTLS {
+		return nil, fmt.Errorf("%w: certificate not of type kubernetes.io/tls", errors.ErrSecretFormatError)
+	}
+
+	cert, ok := secret.Data[corev1.TLSCertKey]
+	if !ok {
+		return nil, fmt.Errorf("%w: certificate missing tls.crt", errors.ErrSecretFormatError)
+	}
+
+	key, ok := secret.Data[corev1.TLSPrivateKeyKey]
+	if !ok {
+		return nil, fmt.Errorf("%w: certifcate missing tls.key", errors.ErrSecretFormatError)
+	}
+
+	certificate, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &certificate, nil
+}
+
 // ApplyTLSClientConfig loads op a client certificate if one is configured and applies
 // it to the provided TLS configuration.
 func (o *HTTPClientOptions) ApplyTLSClientConfig(ctx context.Context, cli client.Client, config *tls.Config) error {
@@ -113,33 +145,77 @@ func (o *HTTPClientOptions) ApplyTLSClientConfig(ctx context.Context, cli client
 		return nil
 	}
 
-	secret := &corev1.Secret{}
-
-	if err := cli.Get(ctx, client.ObjectKey{Namespace: o.secretNamespace, Name: o.secretName}, secret); err != nil {
-		return err
-	}
-
-	if secret.Type != corev1.SecretTypeTLS {
-		return fmt.Errorf("%w: certificate not of type kubernetes.io/tls", errors.ErrSecretFormatError)
-	}
-
-	cert, ok := secret.Data[corev1.TLSCertKey]
-	if !ok {
-		return fmt.Errorf("%w: certificate missing tls.crt", errors.ErrSecretFormatError)
-	}
-
-	key, ok := secret.Data[corev1.TLSPrivateKeyKey]
-	if !ok {
-		return fmt.Errorf("%w: certifcate missing tls.key", errors.ErrSecretFormatError)
-	}
-
-	certificate, err := tls.X509KeyPair(cert, key)
+	certificate, err := o.loadTLSCertificate(ctx, cli)
 	if err != nil {
 		return err
 	}
 
 	config.Certificates = []tls.Certificate{
-		certificate,
+		*certificate,
+	}
+
+	return nil
+}
+
+// EncodeAndSign takes an arbitrary data type, encodes as JSON, generates a digest and creates
+// a digital signature, then returns a stringified version for verifiable communication from
+// one service to another.  Confidentiality is ensured by the use of TLS.
+func (o *HTTPClientOptions) EncodeAndSign(ctx context.Context, cli client.Client, data any) (string, error) {
+	dataJSON, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+
+	certificate, err := o.loadTLSCertificate(ctx, cli)
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: EC is equally valid and need support.
+	pkey, ok := certificate.PrivateKey.(*rsa.PrivateKey)
+	if !ok {
+		return "", errors.ErrUnsupportedKeyType
+	}
+
+	signingKey := jose.SigningKey{
+		Algorithm: jose.PS512,
+		Key:       pkey,
+	}
+
+	signer, err := jose.NewSigner(signingKey, nil)
+	if err != nil {
+		return "", err
+	}
+
+	signedData, err := signer.Sign(dataJSON)
+	if err != nil {
+		return "", err
+	}
+
+	return signedData.CompactSerialize()
+}
+
+// VerifyAndDecode checks the payload's signature against the message and decodes the
+// payload into an arbitrary data type.
+func VerifyAndDecode(data any, payload string, certificate *x509.Certificate) error {
+	signedData, err := jose.ParseSigned(payload)
+	if err != nil {
+		return err
+	}
+
+	// TODO: EC is equally valid and need support.
+	key, ok := certificate.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		return errors.ErrUnsupportedKeyType
+	}
+
+	verifiedData, err := signedData.Verify(key)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(verifiedData, data); err != nil {
+		return err
 	}
 
 	return nil

nscaledev/uni-core

go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Masterminds/semver/v3 v3.3.1
 	github.com/brunoga/deep v1.2.4
 	github.com/getkin/kin-openapi v0.132.0
+	github.com/go-jose/go-jose/v4 v4.0.4
 	github.com/go-logr/logr v1.4.2
 	github.com/go-openapi/jsonpointer v0.21.1
 	github.com/spf13/pflag v1.0.6
@@ -71,6 +72,7 @@ require (
 	go.opentelemetry.io/proto/otlp v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect

pkg/client/http.go
@@ -18,10 +18,13 @@ package client
 
 import (
 	"context"
+	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"fmt"
 
+	jose "github.com/go-jose/go-jose/v4"
 	"github.com/spf13/pflag"
 
 	"github.com/unikorn-cloud/core/pkg/errors"
@@ -106,6 +109,35 @@ func (o *HTTPClientOptions) AddFlags(f *pflag.FlagSet) {
 	f.StringVar(&o.secretName, "client-certificate-name", o.secretName, "Client certificate secret name.")
 }
 
+func (o *HTTPClientOptions) loadTLSCertificate(ctx context.Context, cli client.Client) (*tls.Certificate, error) {
+	secret := &corev1.Secret{}
+
+	if err := cli.Get(ctx, client.ObjectKey{Namespace: o.secretNamespace, Name: o.secretName}, secret); err != nil {
+		return nil, err
+	}
+
+	if secret.Type != corev1.SecretTypeTLS {
+		return nil, fmt.Errorf("%w: certificate not of type kubernetes.io/tls", errors.ErrSecretFormatError)
+	}
+
+	cert, ok := secret.Data[corev1.TLSCertKey]
+	if !ok {
+		return nil, fmt.Errorf("%w: certificate missing tls.crt", errors.ErrSecretFormatError)
+	}
+
+	key, ok := secret.Data[corev1.TLSPrivateKeyKey]
+	if !ok {
+		return nil, fmt.Errorf("%w: certifcate missing tls.key", errors.ErrSecretFormatError)
+	}
+
+	certificate, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &certificate, nil
+}
+
 // ApplyTLSClientConfig loads op a client certificate if one is configured and applies
 // it to the provided TLS configuration.
 func (o *HTTPClientOptions) ApplyTLSClientConfig(ctx context.Context, cli client.Client, config *tls.Config) error {
@@ -113,33 +145,77 @@ func (o *HTTPClientOptions) ApplyTLSClientConfig(ctx context.Context, cli client
 		return nil
 	}
 
-	secret := &corev1.Secret{}
-
-	if err := cli.Get(ctx, client.ObjectKey{Namespace: o.secretNamespace, Name: o.secretName}, secret); err != nil {
-		return err
-	}
-
-	if secret.Type != corev1.SecretTypeTLS {
-		return fmt.Errorf("%w: certificate not of type kubernetes.io/tls", errors.ErrSecretFormatError)
-	}
-
-	cert, ok := secret.Data[corev1.TLSCertKey]
-	if !ok {
-		return fmt.Errorf("%w: certificate missing tls.crt", errors.ErrSecretFormatError)
-	}
-
-	key, ok := secret.Data[corev1.TLSPrivateKeyKey]
-	if !ok {
-		return fmt.Errorf("%w: certifcate missing tls.key", errors.ErrSecretFormatError)
-	}
-
-	certificate, err := tls.X509KeyPair(cert, key)
+	certificate, err := o.loadTLSCertificate(ctx, cli)
 	if err != nil {
 		return err
 	}
 
 	config.Certificates = []tls.Certificate{
-		certificate,
+		*certificate,
+	}
+
+	return nil
+}
+
+// EncodeAndSign takes an arbitrary data type, encodes as JSON, generates a digest and creates
+// a digital signature, then returns a stringified version for verifiable communication from
+// one service to another.  Confidentiality is ensured by the use of TLS.
+func (o *HTTPClientOptions) EncodeAndSign(ctx context.Context, cli client.Client, data any) (string, error) {
+	dataJSON, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+
+	certificate, err := o.loadTLSCertificate(ctx, cli)
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: EC is equally valid and need support.
+	pkey, ok := certificate.PrivateKey.(*rsa.PrivateKey)
+	if !ok {
+		return "", errors.ErrUnsupportedKeyType
+	}
+
+	signingKey := jose.SigningKey{
+		Algorithm: jose.PS512,
+		Key:       pkey,
+	}
+
+	signer, err := jose.NewSigner(signingKey, nil)
+	if err != nil {
+		return "", err
+	}
+
+	signedData, err := signer.Sign(dataJSON)
+	if err != nil {
+		return "", err
+	}
+
+	return signedData.CompactSerialize()
+}
+
+// VerifyAndDecode checks the payload's signature against the message and decodes the
+// payload into an arbitrary data type.
+func VerifyAndDecode(data any, payload string, certificate *x509.Certificate) error {
+	signedData, err := jose.ParseSignedCompact(payload, []jose.SignatureAlgorithm{jose.PS512})
+	if err != nil {
+		return err
+	}
+
+	// TODO: EC is equally valid and need support.
+	key, ok := certificate.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		return errors.ErrUnsupportedKeyType
+	}
+
+	verifiedData, err := signedData.Verify(key)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(verifiedData, data); err != nil {
+		return err
 	}
 
 	return nil