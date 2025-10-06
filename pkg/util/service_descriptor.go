b13197805c7d37d5f43290024d419e758fae6dbd
Simon Murray <spjmurray@yahoo.co.uk>
2025-10-06 10:45:49 +0100

    Abstract Application Information
    Creates a common structure for passing around application specifc naming
    and versioning information to shared libraries.

---
+++ pkg/util/service_descriptor.go
@@ -0,0 +1,29 @@
+/*
+Copyright 2025 the Unikorn Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+// ServiceDescriptor is used to define a common format for application
+// information.  This is typically used to initialize logging and IPC
+// so we can see who instigated a request.
+type ServiceDescriptor struct {
+	// Name is the name of the service.
+	Name string
+	// Version is the version of the service.
+	Version string
+	// Revision is the revision of the service (typically a Git SHA).
+	Revision string
+}
