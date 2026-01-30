cbc1c553d9bf282d9bc3fa88afa0b808c4f41907
Simon Murray <spjmurray@yahoo.co.uk>
2026-01-30 12:31:06 +0000

    Extend Error Introspection (#245)
    Adds in helper functions for every HTTP error type to validate that it's
    an error that will generate an HTTP error code, and it's the right code.
    This is primarily invaluable for testing as the error internals are
    currently unexported.
    
    Also moves the error propagation stuff to server errors rather than
    having it in a separate package as they are tightly coupled, but in a
    backward compatible way.  This facilitates easier changes in the future.

--- pkg/server/errors/errors_test.go
+++ pkg/server/errors/errors_test.go
@@ -31,36 +31,12 @@ import (
 )
 
 const (
-	messageFixture = "Hello World!"
+	messageFixture = "this is a test"
 )
 
 var (
 	errFixture = goerrors.New("fail")
 )
-
-// validate ensures codes, headers and the body is correctly populated.
-func validate(t *testing.T, w *httptest.ResponseRecorder, code int, header http.Header, errorString openapi.ErrorError, description string) {
-	t.Helper()
-
-	require.Equal(t, code, w.Code)
-	require.Equal(t, header, w.Header())
-
-	var body openapi.Error
-
-	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &body))
-	require.Equal(t, errorString, body.Error)
-	require.NotEmpty(t, body.ErrorDescription)
-
-	if description != "" {
-		require.Equal(t, description, body.ErrorDescription)
-	}
-}
-
-func request(t *testing.T) *http.Request {
-	t.Helper()
-
-	return httptest.NewRequestWithContext(t.Context(), http.MethodGet, "https://acme.corp", nil)
-}
 
 func defaultheader() http.Header {
 	h := http.Header{}
@@ -68,6 +44,40 @@ func defaultheader() http.Header {
 	h.Add("Cache-Control", "no-cache")
 
 	return h
+}
+
+type testCase struct {
+	name        string
+	f           func() *errors.Error
+	code        int
+	validator   func(error) bool
+	header      http.Header
+	errorString openapi.ErrorError
+	description string
+}
+
+// validate ensures codes, headers and the body is correctly populated.
+func (tc *testCase) validate(t *testing.T, w *httptest.ResponseRecorder) {
+	t.Helper()
+
+	require.Equal(t, tc.code, w.Code)
+	require.Equal(t, tc.header, w.Header())
+
+	var body openapi.Error
+
+	require.NoError(t, json.Unmarshal(w.Body.Bytes(), &body))
+	require.Equal(t, tc.errorString, body.Error)
+	require.NotEmpty(t, body.ErrorDescription)
+
+	if tc.description != "" {
+		require.Equal(t, tc.description, body.ErrorDescription)
+	}
+}
+
+func request(t *testing.T) *http.Request {
+	t.Helper()
+
+	return httptest.NewRequestWithContext(t.Context(), http.MethodGet, "https://acme.corp", nil)
 }
 
 // TestDefault tests a default error is handled as a 500.
@@ -78,36 +88,42 @@ func TestDefault(t *testing.T) {
 
 	errors.HandleError(w, request(t), errFixture)
 
-	validate(t, w, http.StatusInternalServerError, defaultheader(), openapi.ServerError, "")
+	test := &testCase{
+		code:        http.StatusInternalServerError,
+		header:      defaultheader(),
+		errorString: openapi.ServerError,
+	}
+
+	test.validate(t, w)
 }
 
 // TestNoContext tests handlers that provide no further context.
 func TestNoContext(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name        string
-		f           func() *errors.Error
-		code        int
-		header      http.Header
-		errorString openapi.ErrorError
-	}{
+	tests := []testCase{
 		{
 			name:        "NotFound",
 			f:           errors.HTTPNotFound,
 			code:        http.StatusNotFound,
+			header:      defaultheader(),
+			validator:   errors.IsHTTPNotFound,
 			errorString: openapi.NotFound,
 		},
 		{
 			name:        "MethodNotAllowed",
 			f:           errors.HTTPMethodNotAllowed,
 			code:        http.StatusMethodNotAllowed,
+			header:      defaultheader(),
+			validator:   errors.IsMethodNotAllowed,
 			errorString: openapi.MethodNotAllowed,
 		},
 		{
 			name:        "Conflict",
 			f:           errors.HTTPConflict,
 			code:        http.StatusConflict,
+			header:      defaultheader(),
+			validator:   errors.IsConflict,
 			errorString: openapi.Conflict,
 		},
 	}
@@ -122,13 +138,14 @@ func TestNoContext(t *testing.T) {
 
 			errors.HandleError(w, request(t), test.f())
 
-			header := test.header
-			if header == nil {
-				header = defaultheader()
-			}
-
-			validate(t, w, test.code, header, test.errorString, "")
+			test.validate(t, w)
 		})
+	}
+}
+
+func withContextWrapper(f func(a ...any) *errors.Error) func() *errors.Error {
+	return func() *errors.Error {
+		return f(messageFixture)
 	}
 }
 
@@ -136,41 +153,45 @@ func TestNoContext(t *testing.T) {
 func TestWithContext(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name        string
-		f           func(a ...any) *errors.Error
-		code        int
-		header      http.Header
-		errorString openapi.ErrorError
-	}{
+	tests := []testCase{
 		{
 			name:        "Forbidden",
-			f:           errors.HTTPForbidden,
+			f:           withContextWrapper(errors.HTTPForbidden),
 			code:        http.StatusForbidden,
+			header:      defaultheader(),
+			validator:   errors.IsForbidden,
 			errorString: openapi.Forbidden,
 		},
 		{
 			name:        "RequestEntityTooLarge",
-			f:           errors.HTTPRequestEntityTooLarge,
+			f:           withContextWrapper(errors.HTTPRequestEntityTooLarge),
 			code:        http.StatusRequestEntityTooLarge,
+			header:      defaultheader(),
+			validator:   errors.IsRequestEntityTooLarge,
 			errorString: openapi.RequestEntityTooLarge,
 		},
 		{
 			name:        "InvalidRequest",
-			f:           errors.OAuth2InvalidRequest,
+			f:           withContextWrapper(errors.OAuth2InvalidRequest),
 			code:        http.StatusBadRequest,
+			header:      defaultheader(),
+			validator:   errors.IsBadRequest,
 			errorString: openapi.InvalidRequest,
 		},
 		{
 			name:        "UnprocessableContent",
-			f:           errors.HTTPUnprocessableContent,
+			f:           withContextWrapper(errors.HTTPUnprocessableContent),
 			code:        http.StatusUnprocessableEntity,
+			header:      defaultheader(),
+			validator:   errors.IsUnprocessableContent,
 			errorString: openapi.UnprocessableContent,
 		},
 		{
 			name:        "AccessDenied",
-			f:           errors.OAuth2AccessDenied,
+			f:           withContextWrapper(errors.OAuth2AccessDenied),
 			code:        http.StatusUnauthorized,
+			header:      defaultheader(),
+			validator:   errors.IsAccessDenied,
 			errorString: openapi.AccessDenied,
 		},
 	}
@@ -183,14 +204,60 @@ func TestWithContext(t *testing.T) {
 
 			w := httptest.NewRecorder()
 
-			errors.HandleError(w, request(t), test.f(messageFixture))
+			errors.HandleError(w, request(t), test.f())
 
-			header := test.header
-			if header == nil {
-				header = defaultheader()
-			}
-
-			validate(t, w, test.code, header, test.errorString, messageFixture)
+			test.validate(t, w)
 		})
 	}
+}
+
+type openapiResponseFixture struct {
+	JSON400 *openapi.Error
+}
+
+// TestPropagateError ensures errors are correctly extracted an propagated.
+func TestPropagateError(t *testing.T) {
+	t.Parallel()
+
+	resp := &openapiResponseFixture{
+		JSON400: &openapi.Error{
+			Error:            openapi.InvalidRequest,
+			ErrorDescription: messageFixture,
+		},
+	}
+
+	err := errors.PropagateError(http.StatusBadRequest, resp)
+	require.Error(t, err, "must return an error")
+
+	require.True(t, errors.IsBadRequest(err))
+}
+
+// TestPropagateErrorUnknownCode ensures we can handle something that is unexpected
+// e.g. an ingress going wrong.
+func TestPropagateErrorUnknownCode(t *testing.T) {
+	t.Parallel()
+
+	resp := &openapiResponseFixture{}
+
+	err := errors.PropagateError(http.StatusBadGateway, resp)
+	require.Error(t, err, "must return an error")
+
+	var errorsError *errors.Error
+
+	require.NotErrorAs(t, err, &errorsError, "must not be an API error")
+}
+
+// TestPropagateErrorUnpopulatedCode ensures we can handle something that should be
+// populated but isn't e.g. faulty API error handling.
+func TestPropagateErrorUnpopulatedCode(t *testing.T) {
+	t.Parallel()
+
+	resp := &openapiResponseFixture{}
+
+	err := errors.PropagateError(http.StatusBadRequest, resp)
+	require.Error(t, err, "must return an error")
+
+	var errorsError *errors.Error
+
+	require.NotErrorAs(t, err, &errorsError, "must not be an API error")
 }
